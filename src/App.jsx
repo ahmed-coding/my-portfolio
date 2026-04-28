7220140eaa8d9fe2c3043a47b021b801fd1a1821
Ahmed M Hamzah <71948774+ahmed-coding@users.noreply.github.com>
2026-04-28 03:15:15 +0300

    feat: Update routing to use HashRouter and add redirects configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Design1 from './components/designs/Design1';
 
 function App() {
@@ -6,7 +6,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Design1 />} />
-        {/* <Route path="/1" element={<Design1 />} /> */}
       </Routes>
     </Router>
   );
